019ad58c851d6cbaa97dedcb3268e0e70bf23c73
Purslane <mazhixian25@mails.tsinghua.edu.cn>
2026-08-05 22:35:22 +0800

    Polish frontend badges and admin UI

--- admin-app/src/main.tsx
+++ admin-app/src/main.tsx
@@ -2,4 +2,5 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { EditorApp } from './EditorApp'
 import './styles.css'
+import './overrides.css'
 createRoot(document.getElementById('root')!).render(<React.StrictMode><EditorApp /></React.StrictMode>)
